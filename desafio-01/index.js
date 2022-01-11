d680994633c4fe0f52b82dde66893be68dd30e6e
alan lima <alan.silva1f@gmail.com>
2022-01-10 21:49:52 -0300

    desafio js-intermediario

--- desafio-01/index.js
+++ desafio-01/index.js
@@ -1,10 +1,9 @@
 
-const slide = document.querySelectorAll('.carousel');
-const setaNext = document.querySelector('#next');
-const setaReturn = document.querySelector('#return');
 
 
-setaNext.addEventListener('click', () =>{
-    
-});
+
+
+
+
+
 
